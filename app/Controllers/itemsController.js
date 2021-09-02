8b27a921204171fcbcb4f342dfc725ac7e1a68e4
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-02 16:40:43 -0600

    lost in the sauce

--- app/Controllers/itemsController.js
+++ app/Controllers/itemsController.js
@@ -1,15 +1,33 @@
 import { ProxyState } from "../AppState.js";
 import {itemsService} from "../Services/ItemsService.js";
+import { Item } from "../Models/item.js";
 
-_drawCart(){
-document.getElementById('').innerHTML = template
+function _drawCart(){
+  let template = ''
+  ProxyState.cart.forEach(item => template += Item.Template)
+  ProxyState.cart = ProxyState.cart
+  document.getElementById('app').innerHTML = template
 }
+function _drawItems() {
+  for(let i=0; i = ProxyState.items.length; i++){
+    let template = ''
+    let CItem = PcroxyState.items[i]
+    item.Template
+    document.getElementById('app').innerHTML = template
+  }
+}
+
 
 export class ItemsController {
   constructor() {
+    ProxyState.on('items', _drawItems)
     ProxyState.on('cart', _drawCart)
+    _drawItems()
+  }
+  showItems(){
+    _drawItems() 
   }
   addItems(){
-    itemsService.addItem
+  
   }
 }
